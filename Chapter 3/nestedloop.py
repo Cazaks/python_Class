9ff2e2b21713b53761d5e3a445a2315686eb51ff
Cazaks <calebezak@gmail.com>
2025-05-16 03:47:41 +0100

    First commint

---
+++ Chapter 3/nestedloop.py
@@ -0,0 +1,8 @@
+printing = 10
+for star in range(printing): 
+	for asteric in range(star + 1):
+		print("*", end="")
+
+	print()
+
+
---
+++ Chapter 3/squareandcube.py
@@ -0,0 +1,10 @@
+
+
+print("Number\tSquare\tCube")
+
+for number in range(0, 6):
+	square = number ** 2
+	cube = number ** 3
+	print(f"{number:>6} {square:>6} {cube:>6}")
+
+	
