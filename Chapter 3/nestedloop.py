2ab5f26200804fc6de0885bfd78763193da49437
Cazaks <calebezak@gmail.com>
2025-05-16 04:22:37 +0100

    commiting c and d of the asteric printing

--- Chapter 3/nestedloop.py
+++ Chapter 3/nestedloop.py
@@ -13,3 +13,25 @@ for star in range(printing):
 
 	print()
 
+printing = 10
+for star in range(printing):
+	print(" ", end="") 
+	for asteric in range(star+1):
+		print(" ", end="") 	
+	for asteric in range(star, printing):
+		print("*", end="")
+	
+	print()
+
+print()
+printing = 10
+for star in range(printing):
+	print(" ", end="") 
+	for asteric in range(star, printing):
+		print(" ", end="") 	
+	for asteric in range(star + 1):
+		print("*", end="")
+	
+	print()
+
+
