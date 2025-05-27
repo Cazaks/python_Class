0ecfa2874b414db2dad64d3556ef76414a657794
Cazaks <calebezak@gmail.com>
2025-05-27 14:18:09 +0100

    commit

--- Phase Gate/to_do_list/phase_gate.py
+++ Phase Gate/to_do_list/phase_gate.py
@@ -16,22 +16,49 @@ while menuselect:
 			menuselect = false
 		case 1:
 			taskselect = """
-			1 - Buy groceries
-			2 - task added
+			To-Do List Manager;
+			1. Add task
+			2. View task
+			3. Mark task as complete
+			4. Delete a task
+			5. Exist
 			"""
-			print(taskselect)
-			number1 = int
 			print("Add task")
 			print("Task added")
 		case 2:
+			viewselect = """
+			To-Do List Manager;
+			1. Add task
+			2. View task
+			3. Mark task as complete
+			4. Delete a task
+			5. Exist
+			"""
 			print("view tast") 
 			print("Task viewed")
 
 		case 3: 
+			markselect = """
+			To-Do List Manager;
+			1. Add task
+			2. View task
+			3. Mark task as complete
+			4. Delete a task
+			5. Exist
+			"""
 			print("mark as complete")
 			print("Task completed")
 
 		case 4: 
+			deleteselect = """
+			To-Do List Manager;
+			1. Add task
+			2. View task
+			3. Mark task as complete
+			4. Delete a task
+			5. Exist
+			"""
+
 			print("Delecte task")
 			print("Task delected")
 
